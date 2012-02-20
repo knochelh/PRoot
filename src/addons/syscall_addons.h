a41413ac8fb18e51800b275d7fe634d6139c6249
Christophe Guillon <christophe.guillon@st.com>
2012-02-20 19:35:05 +0100

    Added support for optional addons and contributed a simple trace addon.
    Activate addons with make ENABLE_ADDONS="list of addons", addons sources
    are searched into addons/<addon name>/*.c.
    
    For instance:
      make -C src ENABLE_ADDONS=trace
      env PROOT_ADDON_TRACE=1 proot / echo Hello

---
+++ src/addons/syscall_addons.h
@@ -0,0 +1,72 @@
+/* -*- c-set-style: "K&R"; c-basic-offset: 8 -*-
+ *
+ * This file is part of PRoot.
+ *
+ * Copyright (C) 2010, 2011 STMicroelectronics
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * Author: Cedric VINCENT (cedric.vincent@st.com)
+ *         Christophe Guillon (christophe.guillon@st.com)
+ */
+
+#ifndef SYSCALL_ADDONS_H
+#define SYSCALL_ADDONS_H
+
+#ifdef ENABLE_ADDONS
+
+#include "tracee/info.h"
+
+/**
+ * Records addon information.
+ * Actually the callbacks to enter and exit
+ * syscall processing functions for the addon.
+ * A next field is for internal usage by register/unregister functions.
+ */
+struct addon_info {
+  int (*enter)(struct tracee_info *tracee);
+  int (*exit)(struct tracee_info *tracee);
+  struct addon_info *next;
+};
+
+
+/**
+ * Process the syscall before execution and after default proot processing.
+ * All registered addon enter functions are executed until one returns a negative status.
+ */
+int syscall_addons_enter(struct tracee_info *tracee);
+
+/**
+ * Process the syscall after execution and before default proot processing.
+ * All registered addon exit functions are executed in reverse order until one returns
+ * a negative status.
+ */
+int syscall_addons_exit(struct tracee_info *tracee);
+
+/**
+ * Register a new addon to the processing list.
+ * First in, first executed on syscall entry, last executed on syscall exit.
+ */
+void syscall_addons_register(struct addon_info *addon);
+
+/**
+ * Unregister an addon from the processign list.
+ */
+void syscall_addons_unregister(struct addon_info *addon);
+
+#endif /* ENABLE_ADDONS */
+
+#endif /* SYSCALL_ADDONS_H */
---
+++ src/addons/trace/syscall_trace.c
@@ -0,0 +1,82 @@
+/* -*- c-set-style: "K&R"; c-basic-offset: 8 -*-
+ *
+ * This file is part of PRoot.
+ *
+ * Copyright (C) 2010, 2011 STMicroelectronics
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * Author: Cedric VINCENT (cedric.vincent@st.com)
+ *         Christophe Guillon (christophe.guillon@st.com)
+ */
+
+/**
+ * Simple trace addon for tracing syscall parameters and exit values.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "tracee/info.h"
+#include "tracee/ureg.h"
+#include "syscall/syscall.h"
+#include "addons/syscall_addons.h"
+
+
+/**
+ * Defines OUTPUT macro for all addon outputs.
+ */
+#define OUTPUT(...) fprintf(stderr, __VA_ARGS__);
+#define VERBOSE(...) do { if (verbose) OUTPUT(__VA_ARGS__); } while (0)
+
+
+static int verbose;
+
+/**
+ * Simple output of syscall number and name with arguments.
+ */
+static int addon_enter(struct tracee_info *tracee)
+{
+  VERBOSE("pid %d: syscall(%ld, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx) [0x%lx]\n",
+	  tracee->pid, tracee->sysnum,
+	  peek_ureg(tracee, SYSARG_1), peek_ureg(tracee, SYSARG_2),
+	  peek_ureg(tracee, SYSARG_3), peek_ureg(tracee, SYSARG_4),
+	  peek_ureg(tracee, SYSARG_5), peek_ureg(tracee, SYSARG_6),
+	  peek_ureg(tracee, STACK_POINTER));
+  return 0;
+}
+
+
+/**
+ * Simple output of syscall result.
+ */
+static int addon_exit(struct tracee_info *tracee)
+{
+  VERBOSE("pid %d:        -> 0x%lx [0x%lx]\n", tracee->pid, 
+	  peek_ureg(tracee, SYSARG_RESULT), peek_ureg(tracee, STACK_POINTER));
+  return 0;
+}
+
+
+/**
+ * Register the current addon through a constructor function.
+ */
+static struct addon_info addon = { &addon_enter, &addon_exit };
+
+static void __attribute__((constructor)) register_addon(void)
+{
+  syscall_addons_register(&addon);
+  verbose = getenv("PROOT_ADDON_TRACE") != NULL;
+}
